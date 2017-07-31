11c97696b932db3d69c782f5051b90b3bdafb707
sparklynarwhals <hwd15508@gmail.com>
2017-07-30 21:58:37 -0700

    Completed Text to Speech Test Program 7/30/17

---
+++ src/FreeTTS.java
@@ -0,0 +1,27 @@
+//Text to Speech Test Program
+//8 External Jars Required
+//Mbrola Voices too problematic to use: revisit in the future
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
+public class FreeTTS {
+
+	private static final String VOICENAME="kevin";
+	private Voice voice;
+
+	public FreeTTS(){
+		System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+		VoiceManager voiceManager = VoiceManager.getInstance();
+		voice = voiceManager.getVoice(VOICENAME);
+		voice.allocate();
+	}
+	public void speak(String text){
+		voice.speak(text);
+	}
+
+	public static void main(String[] args){
+		FreeTTS tts = new FreeTTS();
+		tts.speak("hi");
+	}
+}
